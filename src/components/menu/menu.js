8a12742408ee45408f85e6eecf0333572f1e788c
amandacogar <63572463+amandacogar@users.noreply.github.com>
2022-03-03 11:01:19 -0800

    pdf-bug-fix

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -3,10 +3,19 @@ import { Image, Button } from "react-bootstrap";
 import bakery from "./bakery.jpg";
 import coffee from "./coffee.jpg";
 import wine from "./wine.jpg";
-import menu from "./menu.pdf";
+import wineMenu from "./WineMenu.pdf";
+import coffeeMenu from "./CoffeeMenu.pdf";
+import foodMenu from "./FoodMenu.pdf";
+
 function Menu() {
-  const openMenu = (e) => {
-    window.open(menu);
+  const openWine = (e) => {
+    window.open(wineMenu);
+  };
+  const openFood = (e) => {
+    window.open(foodMenu);
+  };
+  const openCoffee = (e) => {
+    window.open(coffeeMenu);
   };
   return (
     <section className="section" id="menu">
@@ -18,19 +27,19 @@ function Menu() {
       <div id="menu-options">
         <div className="menu-link">
           <Image src={bakery} alt="" id="bakery" className="menu-image" />
-          <Button className="circle" onClick={openMenu}>
+          <Button className="circle" onClick={openFood}>
             eats
           </Button>
         </div>
         <div className="menu-link">
           <Image src={coffee} alt="" id="coffee" className="menu-image" />
-          <Button className="circle" onClick={openMenu}>
+          <Button className="circle" onClick={openCoffee}>
             coffee
           </Button>
         </div>
         <div className="menu-link">
           <Image src={wine} alt="" id="wine" className="menu-image" />
-          <Button className="circle" onClick={openMenu}>
+          <Button className="circle" onClick={openWine}>
             wine
           </Button>
         </div>
